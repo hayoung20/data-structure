dbadf8cb660c038cd15bf74faba909d2b3061465
hayoung20 <74517944+hayoung20@users.noreply.github.com>
2021-04-08 00:09:15 +0900

    Add files via upload

---
+++ week6/week6/ReverseString.java
@@ -0,0 +1,17 @@
+package week6;
+
+import java.util.Scanner;
+
+public class ReverseString {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String str = scanner.nextLine();
+		
+		for(int i = str.length()-1; i>=0; i--)
+			System.out.print(str.charAt(i));
+		
+		scanner.close();
+	}
+
+}
