c19b7b7bfb6546ba8a4aaa7916076602c78777d8
hayoung20 <74517944+hayoung20@users.noreply.github.com>
2021-05-12 01:00:41 +0900

    Add files via upload

---
+++ week11/week11/src/week11/Entry.java
@@ -0,0 +1,16 @@
+package week11;
+
+public class Entry <Key extends Comparable<Key>, Value> {
+	private Key ky;
+	private Value val;
+	public Entry (Key newKey, Value newValue) {// 생성자
+		ky = newKey;
+		val = newValue;
+	}
+	//get메소드 set메소드들
+	public Key getKey()	{return ky;}
+	public Value getValue()	{return val;}
+	public void setKey(Key newKey) {ky = newKey;}
+	public void setValue(Value newValue) {val = newValue;}
+
+}
---
+++ week11/week11/src/week11/HeapMain.java
@@ -0,0 +1,24 @@
+package week11;
+
+public class HeapMain {
+
+	public static void main(String[] args) {
+		int n,item;
+		Heap h = new Heap();
+		
+		h.insertHeap(13);
+		h.insertHeap(8);
+		h.insertHeap(10);
+		h.insertHeap(15);
+		h.insertHeap(20);
+		h.insertHeap(19);
+		
+		h.prinHeap();
+		
+		n = h.getHEapSize();
+		for(int i=1; i<=n; i++) {
+			item = h.deleteHeap();
+			System.out.printf("\ndeleted Item : [%d]", item);
+		}
+	}
+}
